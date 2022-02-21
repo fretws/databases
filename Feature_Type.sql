c96982dd19d68bf6a99f536fed4bf5536dbf1aaa
fretws <fretws@uw.edu>
2022-02-21 12:07:28 -0800

    Modify entries of Feature Type table

--- Feature_Type.sql
+++ Feature_Type.sql
@@ -9,25 +9,15 @@ Values (@name, @descr)
 Commit Transaction
 End
 
-Exec usp_insertFeatureType
-  "House Metric",
-  "A measure of a physical aspect of the property, such as quare footage or acreage"
+Delete From tblFEATURE_TYPE
 
 Exec usp_insertFeatureType
   "Proximity",
   "Denotes a building or attraction that is close to the property"
 
 Exec usp_insertFeatureType
-  "Specialized Room",
-  "A room that has a specific purpose, such as a bathroom, gym, or garage"
-
-Exec usp_insertFeatureType
   "Security",
-  "A home security enhancement"
-
-Exec usp_insertFeatureType
-  "Private Outdoors",
-  "A space outdoors such as a front yard or patio, not shared with other properties"
+  "A community security enhancement"
 
 Exec usp_insertFeatureType
   "Shared Space",
@@ -35,6 +25,10 @@ Exec usp_insertFeatureType
 
 Exec usp_insertFeatureType
   "Crime",
-  "A crime that has occurred on or near the property"
+  "A crime that has occurred near the property"
+
+Exec usp_insertFeatureType
+  "Nature",
+  "The type of flora or fauna present in the community"
 
 Select * From tblFEATURE_TYPE
