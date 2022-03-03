3dc5a725ccca14c9714838510dde1d99b153a510
fretws <fretws@uw.edu>
2022-03-03 01:11:11 -0800

    Fix IsOccupiedByOnlyOne in more ways than one

--- Business_Rules.sql
+++ Business_Rules.sql
@@ -1,4 +1,4 @@
--- You can only have one event associated with a given property that has the event type 'Rent' or 'Purchase' at any given time. This means that there can only be one user who rents/owns the property at any given time.
+-- You can only have one user who rents/owns the property at any given time.
 
 Create Function fn_IsOccupiedByOnlyOne()
 Returns INT
@@ -7,14 +7,15 @@ Begin
 Declare @Ret INT = 0
 If Exists (
   Select *
-  From tblEVENT_TYPE ET
-    Join tblEVENT E On E.EventTypeID = ET.EventTypeID
-    Join tblPROPERTY_USER PU On PU.PropertyUserID = E.PropertyUserID
-    Join tblProperty P On P.PropertyID = PU.PropertyUserID
-  Where E.EventName = 'Purchase property'
-     Or E.EventName = 'Rent property'
+  From tblPROPERTY_USER PU
+    Join tblPROPERTY P On P.PropertyID = PU.PropertyID
+    Join tblUSER U On U.UserID = PU.UserID
+    Join tblEvent E On E.PropertyUserID = PU.PropertyUserID
+    Join tblEvent_Type ET On ET.EventTypeID = E.EventTypeID
+  Where ET.EventTypeName = 'Purchase property'
+    Or  ET.EventTypeName = 'Rent property'
   Group By P.PropertyID
-  Having COUNT(Distinct E.EventID) > 1
+  Having COUNT(Distinct U.UserID) > 1
 ) Set @Ret = 1
 Return @Ret
 End
@@ -39,7 +40,7 @@ If Exists (
   From tblEVENT_TYPE ET
     Join tblEVENT E On E.EventTypeID = ET.EventTypeID
     Join tblPROPERTY_USER PU On PU.PropertyUserID = E.PropertyUserID
-    Join tblProperty P On P.PropertyID = PU.PropertyUserID
+    Join tblProperty P On P.PropertyID = PU.PropertyID
   Where E.EventName = 'List for rent'
      Or E.EventName = 'List for purchase'
   Group By P.PropertyID
